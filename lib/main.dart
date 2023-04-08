8bbbc965c62510ce14390c692573218ffb33cd89
kuber somaiya <kubersomaiya@gmail.com>
2023-04-08 13:46:42 +0530

    Firebase Integration Failed

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
 import '../screens/home-screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  // SharedPreferences related code here
+  runApp(MyApp());
 }
 
+// void main(){
+
+//   runApp(MyApp());
+// }
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  // const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Lunatic Bytes',
-        home: HomeScreen(),);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Lunatic Byte',
+      home: HomeScreen(),
+    );
   }
 }
